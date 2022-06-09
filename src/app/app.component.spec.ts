6430336ab97e4ec19c011f3659c0dfcba2fe4b27
Sandesh Margaj <sandeshmargaj1988@gmail.com>
2022-06-09 12:49:06 +0530

    test case updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,6 +27,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('a')?.textContent).toContain('Users');
+    expect(compiled.querySelector('#userlink')?.textContent).toContain('Users');
   });
 });
